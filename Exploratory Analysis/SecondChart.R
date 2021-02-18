2abac7bc601aec63d5aaab35e908d7d989f2ae19
marcodong0815 <jdong2@uw.edu>
2021-02-17 23:46:45 -0500

    2ND CHART

--- Exploratory Analysis/SecondChart.R
+++ Exploratory Analysis/SecondChart.R
@@ -1,1 +1,14 @@
+#this chart ranked the change of the companies
+#we can see what companies have their prices increased or decreased the most
 
+
+library(stringr)
+library(tidyverse)
+library(tidyr)
+library(ggplot2)
+
+toptech <- read.csv("TSL.csv", header = TRUE)
+
+candname<- ggplot(toptech,aes(x=reorder(Name,Change),y=Change,fill="")) + 
+  geom_bar(stat='identity')+labs(y="change",x="Names")
+candname + coord_flip()
