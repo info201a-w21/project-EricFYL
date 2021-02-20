bb5c76e742a7f2f64042f1e01370f3f067d319b7
mrcodong0815 <jdong2@uw.edu>
2021-02-19 23:41:23 -0500

    chart2

--- Exploratory Analysis/SecondChart.R
+++ Exploratory Analysis/SecondChart.R
@@ -10,5 +10,8 @@ library(ggplot2)
 toptech <- read.csv("TSL.csv", header = TRUE)
 
 candname<- ggplot(toptech,aes(x=reorder(Name,Change),y=Change,fill="")) + 
-  geom_bar(stat='identity')+labs(y="change",x="Names")
+  geom_bar(stat='identity')+labs(y="price change",x=" company names")+
+  ggtitle("Price changes rank")+
+  theme(text = element_text(size=7.5))+
+   scale_x_discrete(labels = abbreviate)
 candname + coord_flip()
