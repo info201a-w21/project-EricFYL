76c1b90cd3650cc98b26e921135b8918ed8d3c40
mrcodong0815 <jdong2@uw.edu>
2021-02-28 23:23:45 -0500

    .

--- Exploratory Analysis/SecondChart.R
+++ Exploratory Analysis/SecondChart.R
@@ -1,5 +1,5 @@
-#this chart ranked the change of the companies
-#we can see what companies have their prices increased or decreased the most
+# this chart ranked the change of the companies
+# we can see what companies have their prices increased or decreased the most
 
 
 library(stringr)
@@ -9,9 +9,10 @@ library(ggplot2)
 
 toptech <- read.csv("TSL.csv", header = TRUE)
 
-candname<- ggplot(toptech,aes(x=reorder(Name,Change),y=Change,fill="")) + 
-  geom_bar(stat='identity')+labs(y="price change",x=" company names")+
-  ggtitle("Price changes rank")+
-  theme(text = element_text(size=7.5))+
-   scale_x_discrete(labels = abbreviate)
+candname <- ggplot(toptech, aes(x = reorder(Name, Change), y = Change)) +
+  geom_bar(stat = "identity", fill = "blue") +
+  labs(y = "price change", x = " company names") +
+  ggtitle("Price changes for the top 100 tech companies") +
+  theme(text = element_text(size = 7.5)) +
+  scale_x_discrete(labels = abbreviate)
 candname + coord_flip()
