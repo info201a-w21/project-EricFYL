18ef64cc0ce190535be53f32c6c9dab169f2585b
Sam Cooke <samcooke@Sams-MacBook-Air-2.local>
2021-03-14 15:42:21 -0700

    intro+conclusion

--- app/app_server.R
+++ app/app_server.R
@@ -12,7 +12,7 @@ lint("app_server.R")
 stock_data <- read.csv("TSLDATA.csv")
 
 server <- function(input, output) {
-  output$stockData <- renderPlotly({
+  output$stock_data1 <- renderPlotly({
     select_stock <- input$stock
 
     stock_data_choice <- stock_data %>%
@@ -20,11 +20,11 @@ server <- function(input, output) {
       filter(Name == select_stock) %>%
       select(date, Name, open, close, high, low)
 
-    mainPlot <- stock_data_choice
+    main_plot <- stock_data_choice
 
     colorfun <- input$color
 
-    mainPlot <- mainPlot %>%
+    main_plot <- main_plot %>%
       plot_ly(
         x = ~date,
         type = "candlestick",
@@ -41,7 +41,7 @@ server <- function(input, output) {
 
 
 
-    mainPlot
+    main_plot
   })
 
   output$stockhighlow <- renderPlotly({
@@ -55,6 +55,7 @@ server <- function(input, output) {
 
     second_plot <- stock_data_choice
 
+    # Creating plot
     second_plot <- second_plot %>%
       plot_ly(
         x = ~low,
@@ -73,34 +74,27 @@ server <- function(input, output) {
   output$third_plot <- renderPlotly({
     select_stock_3 <- input$stock3
     select_moving_avg1 <- input$moving_avg1
+    select_moving_avg2 <- input$moving_avg2
 
-    watchlist_data <- stock_data %>%
-      group_by(Name) %>%
-      select(date, Name, open, close, high, low) %>%
-      mutate(
-        fifty_day_MA = rollmean(close, k = 50, fill = NA, align = "right"),
-        two_hundred_day_MA = rollmean(close, k = 200, fill = NA, align = "right")
-      )
 
-    watchlist_data <- watchlist_data %>%
-      filter(date == "2018-02-07") %>%
-      mutate(moving_average_ratio = two_hundred_day_MA / fifty_day_MA) %>%
-      select(Name, moving_average_ratio) %>%
-      arrange(abs(moving_average_ratio - 1))
-
-    SMA_data <- stock_data %>%
+    # Mutating the data to add moving averages
+    # Moving averages were calculated with a library
+    sma_data <- stock_data %>%
       filter(Name == select_stock_3) %>%
       select(date, Name, open, close, high, low) %>%
       mutate(
-        ten_day_MA = rollmean(close, k = 10, fill = NA, align = "right"),
-        twenty_day_MA = rollmean(close, k = 20, fill = NA, align = "right"),
-        fifty_day_MA = rollmean(close, k = 50, fill = NA, align = "right"),
-        hundred_day_MA = rollmean(close, k = 100, fill = NA, align = "right"),
-        two_hundred_day_MA = rollmean(close, k = 200, fill = NA, align = "right")
+        moving_avg_1 = rollmean(close,
+          k = as.numeric(select_moving_avg1),
+          fill = NA, align = "right"
+        ),
+        moving_avg_2 = rollmean(close,
+          k = as.numeric(select_moving_avg2),
+          fill = NA, align = "right"
+        )
       )
 
-
-    third_plot <- SMA_data %>%
+    # Creating plot
+    third_plot <- sma_data %>%
       plot_ly(
         x = ~date,
         type = "candlestick",
@@ -108,13 +102,43 @@ server <- function(input, output) {
         close = ~close,
         high = ~high,
         low = ~low
+      ) %>%
+      layout(
+        title = "Moving Average Chart",
+        xaxis = list(title = "Date"),
+        yaxis = list(title = "Stock Price")
       )
 
+    # Adding Moving Average lines
     third_plot <- third_plot %>%
-      add_lines(y = ~fifty_day_MA, name = "50 Day Moving Average")
+      add_lines(
+        y = ~moving_avg_1,
+        name = "Moving Average 1"
+      )
     third_plot <- third_plot %>%
-      add_lines(y = ~two_hundred_day_MA, name = "200 Day Moving Average")
+      add_lines(y = ~moving_avg_2, name = "Moving Average 2")
+    third_plot
   })
 
+  # Creating the watchlist dataframe by
+  # adding the two most important moving averages
+  watchlist_data <- stock_data %>%
+    group_by(Name) %>%
+    select(date, Name, open, close, high, low) %>%
+    mutate(
+      fifty_day_MA = rollmean(close, k = 50, fill = NA, align = "right"),
+      two_hundred_day_MA = rollmean(close,
+        k = 200,
+        fill = NA, align = "right"
+      )
+    )
+  # Filtering by the most recent date because that's what we want to analyze,
+  # creating a new column with the ratio between moving averages, and
+  # arranging the by the closest to a 1:1 ratio
+  watchlist_data <- watchlist_data %>%
+    filter(date == "2018-02-07") %>%
+    mutate(moving_average_ratio = two_hundred_day_MA / fifty_day_MA) %>%
+    select(Name, moving_average_ratio) %>%
+    arrange(abs(moving_average_ratio - 1))
   output$table <- renderTable(watchlist_data)
 }
--- app/app_ui.R
+++ app/app_ui.R
@@ -10,14 +10,12 @@ lint("app_ui.R")
 
 stock_data <- read.csv("TSLDATA.csv")
 
+# choices for 2nd page widget
 chosen_stock <- unique(stock_data$Name)
+
+# choices for 3rd page widget
 chosen_stock_pg3 <- unique(stock_data$Name)
 
-moving_average_choices <- c(
-  "ten_day_MA", "twenty_day_MA",
-  "fifty_day_MA", "hundred_day_MA",
-  "two_hundred_day_MA"
-)
 
 # Making the color choices for the first page
 color_choices <- c(
@@ -31,8 +29,35 @@ page_one <- tabPanel(
   "Introduction Page",
 
   titlePanel("Introduction"),
+  p("     With the movement in the stock market in the past few months,
+    we thought that it might be interesting to analyze some of the
+    patterns that occur in the data. Analyzing historic stock data
+    is common in the financial sector, and is called technical
+    analysis, or TA. There are a variety of indicators that people use
+    to find a good entry point in different stocks, so we decided to take
+    a look at those and try it ourselves."),
+  p("     The first of indicators we took a look at is called the ."),
+  p("     The second indicator we took a look at is called a moving average, 
+    which is an average of the stock price over a certain period of time.
+    These can be short-term, like over the span of a couple days, which makes
+    the line more jagged and follow every single change in the stock. They
+    can also be longer term, like over the span of 200 days or more.
+    These longer tend to be less changed by daily movements and are more
+    smooth, which makes sense because each data point has less potential to
+    change the entire average. What is interesting is when two cross,
+    which is either called a golden cross or a death cross. The reason for
+    this being that they are both long term bullish and bearish indicators
+    respectively. When a short term moving average crosses the longer 
+    moving average on an uptrend, this is called a golden cross, and indicates
+    that selling is mostly depleted and that there is a long bull market
+    incoming. The death cross is the opposite of this, and indicates a bear
+    market for that particular security. In the second page of analysis,
+    we take a look at the moving averages and specifically which ones are
+    close."),
+  p("")
 )
 
+# 2nd page
 page_two <- tabPanel(
   "Second Page",
   titlePanel("Candlestick Chart"),
@@ -52,12 +77,14 @@ page_two <- tabPanel(
 
     mainPanel(
       h3("Stock Data"),
-      plotlyOutput("stockData"),
+      plotlyOutput("stock_data1"),
       plotlyOutput("stockhighlow")
     )
   )
 )
 
+
+# 3rd page
 page_three <- tabPanel(
   "Third Page",
   titlePanel("Moving Averages Plot"),
@@ -65,13 +92,18 @@ page_three <- tabPanel(
     sidebarPanel(
       selectInput(
         inputId = "stock3",
-        label = "Choose a stock",
+        label = "Please Select a Stock",
         choices = chosen_stock_pg3
       ),
-      selectInput(
+      sliderInput(
         inputId = "moving_avg1",
-        label = "choose moving averages ",
-        choices = moving_average_choices
+        label = "Please Select a Moving Average:",
+        min = 1, max = 500, value = 50
+      ),
+      sliderInput(
+        inputId = "moving_avg2",
+        label = "Please Select a Second Moving Average:",
+        min = 1, max = 500, value = 200
       )
     ),
 
@@ -82,11 +114,37 @@ page_three <- tabPanel(
   )
 )
 
+page_four <- tabPanel(
+  "Fourth Page",
+  titlePanel("plot")
+)
 
+page_five <- tabPanel(
+  "Summary Takeaways",
 
+  titlePanel("Takeaways"),
+  p(""),
+  p("With the dataframe that we created, we are able to see which stocks
+    have 50 Day and 200 Day moving averages that are closest together, and
+    with the chart we are able to look at those stocks and see if they are
+    on the uptrend or downtrend. With up to date data, we could use this to
+    find good entry points on stocks that are on the uptrend, or to short
+    stocks that have made a death cross. Using one of the top tickers on our
+    list K as an example, we can see whether the uptrend continued because 
+    the latest datapoint was from 2018, to see whether this was a good buy.
+    If we bought this at the closing price of 66.84 on 02/07/2018, we could
+    have had the opportunity to sell at 75 only a couple months later,
+    which is a decent return. While I would have liked to see a larger
+    % change, this shows that it can be a powerful tool."),
+  p("")
+)
+
+# Building navbar with all pages
 ui <- navbarPage(
   "Final Deliverable",
   page_one,
   page_two,
-  page_three
+  page_three,
+  page_four,
+  page_five
 )
